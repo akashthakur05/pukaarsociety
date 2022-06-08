28c98b6a1f962aea48622fa474a70c991bd075af
Akash Thakur <akashkumarsngh9@gmail.com>
2022-06-08 20:04:33 +0530

    first commit

---
+++ src/app/component/aside/aside.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aside-component',
+  templateUrl: './aside.component.html',
+  styleUrls:['./aside.component.sass']
+})
+
+export class AsideComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
